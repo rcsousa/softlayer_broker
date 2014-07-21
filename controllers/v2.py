db237b404e9c66e252728dce709b582f56b76a9c
Ricardo Coelho de Sousa <ricardo.c.sousa@gmail.com>
2014-07-21 00:45:42 +0000

    first commit

--- controllers/v2.py
+++ controllers/v2.py
@@ -5,14 +5,14 @@ def catalog():
     from gluon.tools import Auth
     import yaml
     import json
-    with open('/settings_sco.yml', 'r') as f:
+    with open('/settings_softlayer.yml', 'r') as f:
         yaml_settings = yaml.load(f)
     json_settings = json.dumps(yaml_settings)
     data = json.loads(json_settings)
     return data['catalog']
 
 @auth.requires_login()
-def test_sco():
+def test():
     import uuid, os, yaml, json, docker, requests
     id = request.args
     return id[0]
@@ -20,42 +20,44 @@ def test_sco():
 
 @auth.requires_login()
 def service_instances():
-    import uuid, os, yaml, json, docker, requests
+    import uuid, os, yaml, json, docker, requests, traceback
+    from SoftLayer import VSManager, Client
     id = request.args
-    with open('/settings_sco.yml', 'r') as f:
+    with open('/settings_softlayer.yml', 'r') as f:
         yaml_settings = yaml.load(f)
     json_settings = json.dumps(yaml_settings)
     data = json.loads(json_settings)
-    sco_api_url = data['pattern']['api_ep'][0]['url']
-    sco_api_user = data['pattern']['api_ep'][0]['user']
-    sco_api_password = data['pattern']['api_ep'][0]['password']
-    sco_api_headers = data['pattern']['api_ep'][0]['headers']
-    with open('/sco_pattern.json', 'r') as json_content_read:
+    sl_api_user = data['pattern']['api_ep'][0]['user']
+    sl_api_key = data['pattern']['api_ep'][0]['api_key']
+    client = Client(username=sl_api_user, api_key=sl_api_key)
+    vs = VSManager(client)
+    with open('/softlayer.json', 'r') as json_content_read:
         build_content_read = json.load(json_content_read)
         json_content_read.close()
 
-    build_content_read['name'] = id[0]
+    build_content_read['hostname'] = id[0]
 
-    with open('/sco_pattern.json', 'w') as json_content_write:
+    with open('/softlayer.json', 'w') as json_content_write:
         json_content_write.write(json.dumps(build_content_read))
         json_content_write.close()
 
-    with open('/sco_pattern.json') as json_content:
-        build_content_load = json.load(json_content)
-        build_content = json.dumps(build_content_load)
+    with open('/softlayer.json') as json_content:
+        build_content = json.load(json_content)
 
     if len(id) == 1:
         if request.env.request_method == "PUT":
-            r = requests.post(sco_api_url+'/resources/virtualSystems', verify=False, auth=(sco_api_user, sco_api_password), headers=sco_api_headers, data=build_content)
-            response = { "dashboard_url": id }
-            return response
+            try:
+                client['Virtual_Guest'].createObject(build_content)
+                response = { "dashboard_url": id }
+                return response
+            except:
+                print traceback.format_exc()
         if request.env.request_method == "DELETE":
             try:
-                r = requests.get(sco_api_url+'/resources/virtualSystems', verify=False, auth=(sco_api_user, sco_api_password), headers=sco_api_headers)
-                for i in r.json():
-                        if str(i['name']) == str(id[0]):
-                                sco_id = str(i['id'])
-                                delete = requests.delete(sco_api_url+'/resources/virtualSystems/'+sco_id, verify=False, auth=(sco_api_user, sco_api_password), headers=sco_api_headers)
+                for i in vs.list_instances():
+                    if str(i['hostname']) == str(id[0]):
+                        virtual_server_id = i['id']
+                vs.cancel_instance(virtual_server_id)
                 response = {}
                 return response
             except:
@@ -66,19 +68,20 @@ def service_instances():
     if len(id) > 1:
         if request.env.request_method == "PUT":
             try:
-                r = requests.get(sco_api_url+'/resources/virtualSystems', verify=False, auth=(sco_api_user, sco_api_password), headers=sco_api_headers)
-                for i in r.json():
-                    if str(i['name']) == str(id[0]):
-                        sco_id = str(i['id'])
-                        s = requests.get(sco_api_url+'/resources/virtualSystems/'+sco_id+'/virtualMachines', verify=False, auth=(sco_api_user, sco_api_password), headers=sco_api_headers)
-                        for data in s.json():
-                            ip_addr = str(data['nics'][0]['ip_address'])
-                response = {"credentials": {"ip_address": ip_addr}}
+                for i in vs.list_instances():
+                    if str(i['hostname']) == str(id[0]):
+                        virtual_server_id = i['id']
+                ip_addr = vs.get_instance(virtual_server_id)['primaryIpAddress']
+                #username = vs.get_instance(virtual_server_id)['networkComponents']['operatingSystem']['passwords'][0]['username']
+                #password = vs.get_instance(virtual_server_id)['networkComponents']['operatingSystem']['passwords'][0]['password']
+                #response = {"credentials": [{"ip_address": ip_addr},{"username": username}, {"password": password}]}
+                response = {"credentials": {"ip_address": ip_addr, "port": "11211"}}
                 return response
             except:
-                raise HTTP(404, "Not Found")
-                response = {}
-                return response
+                return traceback.format_exc()
+                #raise HTTP(404, "Not Found")
+                #response = {}
+                #return response
         if request.env.request_method == "DELETE":
             response = {}
             return response
